6774683f5733889b09c7188bdea608f442c8ddb8
jnannni <49609088+jnannni@users.noreply.github.com>
2025-02-17 22:59:18 +0100

    changed path

--- src/components/Astronaut/Astronaut.tsx
+++ src/components/Astronaut/Astronaut.tsx
@@ -1,6 +1,9 @@
 import { useContext, useEffect, useState } from "react";
 import { InstallationContext } from "../../contexts/installationContext";
 import "./astronaut.css";
+import astronaut from "/assets/Astronaut.png";
+import astronautUn from "/assets/astronaut_undressed.png";
+import rover from "assets/rover.png";
 
 //contains a clickable image that changes whether a user "wears" a space suit or not
 export default function Astronaut() {
@@ -25,13 +28,13 @@ export default function Astronaut() {
                 {installationState.magneticFieldInstalled ? <span>Remove magnetic field</span> : <span>Install magnetic field</span>}
             </button>
             <div className="astro-img-container">
-                <img className={deviceWidth >= 768 ? "astronaut-img" : "hidden"} src={state.spaceSuitOn ? "/assets/Astronaut.png" : "/assets/astronaut_undressed.png"} 
+                <img className={deviceWidth >= 768 ? "astronaut-img" : "hidden"} src={state.spaceSuitOn ? astronaut : astronautUn} 
                 onClick={() => dispatch({type: "WEAR_SUIT"})} />
                 <span className="material-symbols-outlined">touch_app</span>
             </div>
             <button className={deviceWidth < 768 ? "button" : "hidden"} onClick={() => dispatch({type: "WEAR_SUIT"})}>
                 {state.spaceSuitOn ? <span>Take off your space suit</span> : <span>Wear your space suit</span>}</button>
-            <img className={deviceWidth >= 768 ? "rover-img" : "hidden"} src="/assets/rover.png"/>
+            <img className={deviceWidth >= 768 ? "rover-img" : "hidden"} src={rover}/>
         </div>
     )
 }
